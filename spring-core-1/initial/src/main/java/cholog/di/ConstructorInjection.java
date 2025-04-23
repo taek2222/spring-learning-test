795a4a8a64551c91fb93453e4a1044ac8c693b91
taek2222 <oot309@daum.net>
2025-04-23 21:09:12 +0900

    study: Spring Bean 학습

--- spring-core-1/initial/src/main/java/cholog/di/ConstructorInjection.java
+++ spring-core-1/initial/src/main/java/cholog/di/ConstructorInjection.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ConstructorInjection {
-    private InjectionBean injectionBean;
 
-    /*
-    ConstructorInjection으로 InjectionBean 주입받기
-     */
+    private final InjectionBean injectionBean;
+    
+    public ConstructorInjection(final InjectionBean injectionBean) {
+        this.injectionBean = injectionBean;
+    }
+
     public String sayHello() {
         return injectionBean.hello();
     }
